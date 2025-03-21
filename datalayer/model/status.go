f82411107490b8edcac006ecf3a57052bda90a6c
xmx <xmx@github.com>
2025-03-22 01:44:13 +0800

    流量同步

--- datalayer/model/status.go
+++ datalayer/model/status.go
@@ -16,19 +16,19 @@ type Status struct {
 	Name    string             `bson:"name,omitempty"`
 	Cluster string             `bson:"cluster,omitempty"`
 	Private string             `bson:"private,omitempty"`
-	Time    time.Time          `bson:"time,omitempty"`
-	Status  StatusItem         `bson:"status,omitempty"`
+	Time    time.Time          `bson:"time"`
+	Status  StatusItem         `bson:"status"`
 }
 
 type StatusItem struct {
-	Total  int64 `bson:"total,omitempty"  json:"total"`
-	Deny   int64 `bson:"deny,omitempty"   json:"deny"`
-	S200   int64 `bson:"_200,omitempty"   json:"_200"`
-	S30x   int64 `bson:"_30x,omitempty"   json:"_30x"`
-	S40x   int64 `bson:"_40x,omitempty"   json:"_40x"`
-	S50x   int64 `bson:"_50x,omitempty"   json:"_50x"`
-	S100   int64 `bson:"_100,omitempty"   json:"_100"`
-	S1000  int64 `bson:"_1000,omitempty"  json:"_1000"`
-	S10000 int64 `bson:"_10000,omitempty" json:"_10000"`
-	Sx0000 int64 `bson:"_x0000,omitempty" json:"_x0000"`
+	Total  int64 `bson:"total"  json:"total"`
+	Deny   int64 `bson:"deny"   json:"deny"`
+	S200   int64 `bson:"_200"   json:"_200"`
+	S30x   int64 `bson:"_30x"   json:"_30x"`
+	S40x   int64 `bson:"_40x"   json:"_40x"`
+	S50x   int64 `bson:"_50x"   json:"_50x"`
+	S100   int64 `bson:"_100"   json:"_100"`
+	S1000  int64 `bson:"_1000"  json:"_1000"`
+	S10000 int64 `bson:"_10000" json:"_10000"`
+	Sx0000 int64 `bson:"_x0000" json:"_x0000"`
 }
